c1a75cffa1034419a87fe56c81815af97c021e90
KimSangWook57 <asdewrp@naver.com>
2023-05-23 17:20:02 +0900

    샘플 데이터 업로드

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import io
 import yolov5
-# import tensorflow as tf
+import torch
+import os
 
 from flask import Flask, request, jsonify
 from PIL import Image
@@ -30,7 +31,7 @@ def predict():
         # 모델에 사진을 넣어서 판별
         results = model(im, size=640)
         # 결과를 사진으로 저장
-        results.save()
+        results.save(save_dir=RESULTS_DIR)
         # bbox 값을 딕셔너리에 담아서 json으로 전달
         return jsonify(results.pandas().xyxy[0].to_dict('records'))
     
@@ -43,9 +44,11 @@ def predict():
 if __name__ == '__main__':
     try:
         model = yolov5.load('keremberke/yolov5s-garbage')
+        # model = torch.hub.load('ultralytics/yolov5', 'yolov5s')  # yolov5n - yolov5x6 official model
+        # model = torch.load('best_model.pt', map_location=torch.device('cpu')) # custom model
     except Exception as e:
         print(f'Failed to load model: {str(e)}')
         exit(1)
     
-    app.run(host='0.0.0.0', debug=True, port=5000)
+    app.run(host='0.0.0.0', debug=True, port=55000)
 
