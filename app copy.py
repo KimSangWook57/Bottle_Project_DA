52e8ab21b28585e9b4d0e6023dbb365993121478
KimSangWook57 <asdewrp@naver.com>
2023-05-30 18:41:36 +0900

    커스텀 모델 올리기 연습

--- app copy.py
+++ app copy.py
@@ -11,6 +11,7 @@ import torchvision.transforms as transforms
 
 from flask import Flask, request, jsonify
 from PIL import Image
+from testmodel import Model
 
 app = Flask(__name__)
 model = None
@@ -44,8 +45,13 @@ def predict():
         transform = transforms.ToTensor()
         im_tensor = transform(im)
 
+        # 이미지를 Tensor로 변환
+        transform = transforms.ToTensor()
+        # 이미지 차원을 추가하여 4D Tensor로 변환
+        im_tensor = torch.unsqueeze(transform(im), 0)
         # 모델에 Tensor 이미지를 넣어서 판별
         results = model(im_tensor)
+
         # 결과를 사진으로 저장
         # results.save(save_dir=RESULTS_DIR)
         # image_path = os.path.join(RESULTS_DIR, 'image0.jpg')
@@ -67,49 +73,34 @@ def predict():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-# 모델 정의
-class TheModelClass(nn.Module):
-    def __init__(self):
-        super(TheModelClass, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 97 * 97, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
-
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        # print(x.size())  # conv2의 출력 크기 확인
-        x = x.view(-1, 16 * 97 * 97)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        
-        return x
-
-
 if __name__ == '__main__':
     try:
         # model = yolov5.load('keremberke/yolov5s-garbage')
         # model = torch.hub.load('ultralytics/yolov5', 'yolov5s')  # yolov5n - yolov5x6 official model
         # model = torch.load('best_model.pt')  # custom model
+        # # 모델 초기화
+        # model = TheModelClass()
+
+        # # 옵티마이저 초기화
+        # optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+        # # 모델의 state_dict 출력
+        # print("Model's state_dict:")
+        # for param_tensor in model.state_dict():
+        #     print(param_tensor, "\t", model.state_dict()[param_tensor].size())
+
+        # # 옵티마이저의 state_dict 출력
+        # print("Optimizer's state_dict:")
+        # for var_name in optimizer.state_dict():
+        #     print(var_name, "\t", optimizer.state_dict()[var_name])
+
         # 모델 초기화
-        model = TheModelClass()
+        model = Model()
 
-        # 옵티마이저 초기화
-        optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
-
-        # 모델의 state_dict 출력
-        print("Model's state_dict:")
-        for param_tensor in model.state_dict():
-            print(param_tensor, "\t", model.state_dict()[param_tensor].size())
-
-        # 옵티마이저의 state_dict 출력
-        print("Optimizer's state_dict:")
-        for var_name in optimizer.state_dict():
-            print(var_name, "\t", optimizer.state_dict()[var_name])
+        # 저장된 모델 로드
+        model_path = 'best_model.pt'
+        model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu'))) # cpu load
+        model.eval()
 
     except Exception as e:
         print(f'Failed to load model: {str(e)}')
